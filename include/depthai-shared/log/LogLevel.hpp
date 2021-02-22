617469210a8ccdae38e0566654f64c4ceaefa187
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2021-02-22 12:54:24 +0200

    Add libspdlog v1.5.0 compatibility
    ROS2 uses libspdlog v1.5.0 and when building applications with
    depthai-core, ROS2 picks up newer version of the libspdlog during
    linking process and this will cause segmenation fault during
    initialization of ROS2 node.

--- include/depthai-shared/log/LogLevel.hpp
+++ include/depthai-shared/log/LogLevel.hpp
@@ -6,7 +6,16 @@
 // libraries
 #include <nlohmann/json.hpp>
 
+#if SPDLOG_VERSION < 10601
+#include "spdlog/spdlog.h"
+#endif
+
 namespace dai {
+
+// Work around when using v1.5.0 of SPDLOG library
+#if SPDLOG_VERSION < 10601
+extern spdlog::level::level_enum CurrentLogLevel;
+#endif
 
 // Follows spdlog levels
 enum class LogLevel : std::int32_t { TRACE = 0, DEBUG, INFO, WARN, ERR, CRITICAL, OFF };
